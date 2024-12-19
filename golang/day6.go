fe929c0bbb8c3ebf9d7a37ddea8aea80d1a4fa78
Azka Ali <azkali20@gmail.com>
2024-12-19 11:41:40 +0100

    day6

--- golang/day6.go
+++ golang/day6.go
@@ -2,11 +2,8 @@ package main
 
 import (
 	"fmt"
-	"io"
 	"math/bits"
-	"net/http"
 	"os"
-	"strconv"
 	"strings"
 	"time"
 )
@@ -44,96 +41,15 @@ func day6() {
 		0b0001, // left
 	}
 
-	blockloclist := []int{}
-	startloclist := []int{}
-	dirloclist := []int{}
-
-	// have normal tracing
-	// create some memo to count solution for part 1
-	// for part 2
-	// count every single possible block positioning that will cause a loop
-	// 1. detect loop
-	// 2. ensure no double loop detection
-	n_isloop := 0
-
-	isloop := func(start, cdir, newblock int) bool {
-		// fmt.Println("check isloop", n_isloop)
-		n_isloop++
-		idir := cdir
-		icurr := start
-
-		count := 0
-		countturn := 0
-
-		lvisitMap := make([]int, len(input))
-		_ = lvisitMap
-
-		for {
-			// if count%100 == 0 {
-			// 	res := drawmap_pos(input, visitMap, lvisitMap, icurr, idir)
-			// 	println(res)
-			// 	time.Sleep(100 * time.Millisecond)
-			// }
-			// println("w", ndir, icurr, count)
-			// println("w", cdir, stalrt)
-			if icurr == start && idir == cdir && count > 0 {
-				if count < 10 {
-					println("whatt??", count, countturn)
-				}
-				// println("return")
-				return true
-			}
-			lvisitMap[icurr] |= dirflag[idir]
-
-			inext := icurr + dir[idir]
-			if inext < 0 || inext >= len(input) || input[inext] == '\n' {
-				break
-			}
-
-			if input[inext] == '#' || inext == newblock {
-				idir = (idir + 1) % 4
-				countturn++
-			} else {
-				icurr = inext
-				count++
-			}
-		}
-		// println("return")
-		return false
-	}
-	_ = isloop
-
-	fpcount := 0
-	_ = fpcount
-
-	nexttileblockwillLoop := func(lcurr, lcdir, newblock int) (bool, int, int) {
-		// idir := (lcdir + 1) & 3
+	nexttileblockwillLoop := func(lcurr, lcdir, newblock int) bool {
 		idir := lcdir
 		icurr := lcurr
 
 		lvisitMap := make([]int, len(input))
-		_ = lvisitMap
-
-		count := 0
-		_ = count
 
 		for {
-			// drawmap_pos(input, visitMap, lvisitMap, curr, ndir)
-
-			// if count%100 == 0 {
-			// 	res := drawmap_pos(input, visitMap, lvisitMap, icurr, idir)
-			// 	println(res)
-			// 	time.Sleep(100 * time.Millisecond)
-			// }
-			//          count++
-
-			if (lvisitMap[icurr])&dirflag[idir] > 0 {
-				// if !isloop(icurr, idir, newblock) {
-				// 	fmt.Println("false positive?")
-				// 	fpcount++
-				// 	// return false
-				// }
-				return true, icurr, idir
+			if ((lvisitMap[icurr]))&dirflag[idir] > 0 {
+				return true
 			}
 			lvisitMap[icurr] |= dirflag[idir]
 
@@ -148,15 +64,11 @@ func day6() {
 				icurr = next
 			}
 		}
-		return false, 0, 0
+		return false
 	}
 
 	cdir := 0
 	curr := loc
-
-	sum3 := 0
-	willblockMap2 := make([]bool, len(input))
-	_ = willblockMap2
 
 	for {
 		if visitMap[curr] == 0 {
@@ -170,14 +82,9 @@ func day6() {
 		}
 
 		if !willblockMap[next] {
-			if ok, a, b := nexttileblockwillLoop(loc, 0, next); ok {
+			if nexttileblockwillLoop(loc, 0, next) {
 				willblockMap[next] = true
 				sum2++
-
-				blockloclist = append(blockloclist, next)
-				startloclist = append(startloclist, a)
-				dirloclist = append(dirloclist, b)
-
 			}
 		}
 
@@ -191,70 +98,29 @@ func day6() {
 	cdir = 0
 	curr = loc
 
-	cnt_willblockmap := 0
-	for i := range willblockMap {
-		if willblockMap[i] {
-			cnt_willblockmap++
-		}
-	}
-	fmt.Println("checking, willblockmap: ", cnt_willblockmap)
-	fmt.Println("checking, willblockmap: ", sum3)
-
-	fmt.Println("checking, fp count: ", fpcount)
-	cntfail := 0
-	cntright1 := 0
-	cntright2 := 0
-	for i := range blockloclist {
-		if ok, _, _ := nexttileblockwillLoop(loc, 0, blockloclist[i]); !ok {
-			// fmt.Println("fail", i)
-			cntfail++
-		}
-		if isloop(startloclist[i], dirloclist[i], blockloclist[i]) {
-			// fmt.Println("fail", i)
-			cntright1++
-		}
-		if ok, _, _ := nexttileblockwillLoop(startloclist[i], dirloclist[i], blockloclist[i]); ok {
-			// fmt.Println("fail", i)
-			cntright2++
-		}
-
-		if input[blockloclist[i]] == '#' {
-			println("heyy")
-		}
-		if input[blockloclist[i]] == '^' {
-			println("heyy")
-		}
-
-	}
-
 	fmt.Println("sum1: ", sum1)
-	fmt.Println("sum2 (should be true): ", sum2)
-	fmt.Println("sum2 (should true): ", len(blockloclist))
-	fmt.Println("sum2 (should be 0): ", cntfail)
-	fmt.Println("sum2 (should true): ", sum2-cntfail)
-	fmt.Println("check 2 (should be true): ", cntright1)
-	fmt.Println("check 3 (should be true): ", cntright2)
+	fmt.Println("sum2: ", sum2)
 
-	http.HandleFunc("/day6", func(w http.ResponseWriter, r *http.Request) {
-		res, err := io.ReadAll(r.Body)
-		if err != nil {
-			fmt.Println("error: ", err)
-		}
-		fmt.Println("test", string(res))
+	// http.HandleFunc("/day6", func(w http.ResponseWriter, r *http.Request) {
+	// 	res, err := io.ReadAll(r.Body)
+	// 	if err != nil {
+	// 		fmt.Println("error: ", err)
+	// 	}
+	// 	fmt.Println("test", string(res))
+	//
+	// 	idxRaw := r.URL.Query().Get("idx")
+	//
+	// 	idx, _ := strconv.Atoi(idxRaw)
+	// 	_ = idx
+	//
+	// 	_, err = fmt.Fprintf(w, drawmap(input, visitMap))
+	//
+	// 	if err != nil {
+	// 		fmt.Println("error: ", err)
+	// 	}
+	// })
 
-		idxRaw := r.URL.Query().Get("idx")
-
-		idx, _ := strconv.Atoi(idxRaw)
-		_ = idx
-
-		_, err = fmt.Fprintf(w, drawmap(input, visitMap))
-
-		if err != nil {
-			fmt.Println("error: ", err)
-		}
-	})
-
-	http.Handle("/", http.FileServer(http.Dir(".")))
+	// http.Handle("/", http.FileServer(http.Dir(".")))
 
 	// http.ListenAndServe(":8080", nil)
 }
@@ -284,25 +150,18 @@ func drawmap_pos(m string, visitmapdir, visitmapdir_temp []int, loc, dir int) st
 		if bits.OnesCount(uint(visitmapdir[i])) > 1 {
 			result[i] = '+'
 		} else if visitmapdir[i]&0b1010 > 0 {
-			// result += "<span class=\"text-red-600\">|</span>"
 			result[i] = '|'
 		} else if visitmapdir[i]&0b0101 > 0 {
-			// result += "<span class=\"text-red-600\">-</span>"
 			result[i] = '-'
 		} else if bits.OnesCount(uint(visitmapdir_temp[i])) > 1 {
-			// result += "<span class=\"text-blue-600\">+</span>"
 			result[i] = '+'
 		} else if visitmapdir_temp[i]&0b1010 > 0 {
-			// result += "<span class=\"text-blue-600\">|</span>"
 			result[i] = '|'
 		} else if visitmapdir_temp[i]&0b0101 > 0 {
-			// result += "<span class=\"text-blue-600\">-</span>"
 			result[i] = '-'
 		} else if string(val) == "\n" {
-			// result += "<br/>"
 			result[i] = '\n'
 		} else {
-			// result += string(val)
 			result[i] = byte(val)
 		}
 
@@ -348,17 +207,11 @@ func drawmap2(m string, visitmapdir []int, visitmapdir_temp []int) string {
 	return result
 }
 
-func printMap(m string, visitmapdir []int, visitmap []bool) {
-	_ = visitmapdir
-	_ = visitmap
-
+func printMap(m string, visitmapdir []int) {
 	println(m)
 
 	for i, val := range m {
 		_, _ = i, val
-
-		// fmt.Printf("%v\n", visitmapdir[i])
-		// _ = bits.LeadingZeros
 
 		if bits.OnesCount(uint(visitmapdir[i])) > 1 {
 			fmt.Printf("+")
@@ -369,12 +222,6 @@ func printMap(m string, visitmapdir []int, visitmap []bool) {
 		} else {
 			fmt.Printf("%c", val)
 		}
-
-		// if visitmap[i] {
-		// 	fmt.Printf("O")
-		// } else {
-		// 	fmt.Printf("%c", val)
-		// }
 
 	}
 
